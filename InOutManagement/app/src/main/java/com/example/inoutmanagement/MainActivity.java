1c15882d2d0363cf03bdfc5cdb603457353a00c1
youseokhwan <tjrghks115@naver.com>
2019-12-16 14:25:53 +0900

    연결된 wi-fi 정보 디스플레이 구현

--- InOutManagement/app/src/main/java/com/example/inoutmanagement/MainActivity.java
+++ InOutManagement/app/src/main/java/com/example/inoutmanagement/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.inoutmanagement;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends Activity {
 
@@ -36,6 +42,24 @@ public class MainActivity extends Activity {
                 bluetoothInfo.setVisibility(View.VISIBLE);
             }
         });
+
+        // Wi-fi
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_FINE_LOCATION }, 1);
+
+        WifiManager wifiManager = (WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        int ip = wifiManager.getConnectionInfo().getIpAddress();
+        String ipAddress = String.format("%d.%d.%d.%d", (ip & 0xff), (ip >> 8&0xff), (ip >> 16&0xff), (ip >> 24&0xff));
+
+        String wifiInfoText = "SSID: " + wifiManager.getConnectionInfo().getSSID()
+                + "\nBSSID: " + wifiManager.getConnectionInfo().getBSSID()
+                + "\nIpAddress: " + ipAddress
+                + "\nLinkSpeed: " + wifiManager.getConnectionInfo().getLinkSpeed()
+                + "\nNetworkId: " + wifiManager.getConnectionInfo().getNetworkId()
+                + "\nRssi: " + wifiManager.getConnectionInfo().getRssi();
+
+        wifiInfo.setText(wifiInfoText);
     }
 
 }
